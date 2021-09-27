5966e844a1902f8d9513a44b1896e6f37adfe301
SheremyZX <33012169+SheremyZX@users.noreply.github.com>
2021-09-27 11:51:23 -0500

    Flechaaaaaaaaaaaaa

---
+++ interfacesJZ/animales.php
@@ -0,0 +1,27 @@
+<?php 
+class Animal{
+    public $nombre;
+    public $clase;
+    function __construct($nombre, $clase){
+        $this->nombre=$nombre;
+        $this->clase=$clase;
+    }
+
+
+
+    public function setter($atributo, $valor){
+        $this->$atributo=$valor;
+    }
+
+    public function getter($atributo){
+        return $this->$atributo;
+    }
+}
+
+
+
+
+
+
+
+?>
