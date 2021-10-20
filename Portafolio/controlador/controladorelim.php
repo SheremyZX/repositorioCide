a69e16c44b24ce76b8c4f4955a79c514d006f326
Jezz <jeremizapata89@gmail.com>
2021-10-20 17:20:22 -0500

    Portafolio final y Bitacora

--- Portafolio/controlador/controladorelim.php
+++ Portafolio/controlador/controladorelim.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../vista/estilos.css">
-    <title>modificar</title>
+    <title>Modificar</title>
+    <link rel="stylesheet" type="text/css" href="estilos/estilozzz.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre&family=Orbitron&family=Stick+No+Bills:wght@500&display=swap" rel="stylesheet">
+    
+    <link rel="shortcut icon" href="imagenes/jz.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <?php 
